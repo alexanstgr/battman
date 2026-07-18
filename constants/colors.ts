719cb4b096d7b96924efb35788b2077c8dbc3937
alexanstgr <alexanstgr@gmail.com>
2026-07-18 16:05:55 +0300

    update primary color and background

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,11 +1,11 @@
-const primary = "rgb(255, 42, 0)";
+const primary = "rgb(255, 47, 0)";
 
 const colors = {
   radius: 8,
 
   tint: primary,
 
-  background: "#f3f3f3",
+  background: "#eeeeee",
   foreground: "#151515",
 
   card: "#ffffffef",
@@ -25,18 +25,23 @@ const colors = {
   destructive: "#E53E3E",
   destructiveForeground: "#ff0101",
 
-  border: "#05050517",
+  border: "#05050522",
+  borderWidth: 0.5,
   input: "#ececec",
 
+  shadow: "#ffffff",
+
+  // card borders
+  cardBorderColor: "#c5c5c5",
+  cardBorderWidth: 1,
+
+  // battery level colors
   charged: "#00ce89",
   charging: "#ff9500",
   discharged: "#ff2d2d",
 
   surfaceElevated: "#fffffff4",
-
   tabBar: "#ffffff",
-
-  shadow: "#64646400",
 };
 
 export default colors;
